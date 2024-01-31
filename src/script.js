276f0ff70ea093010e510a8e3322bcb06a73abf7
joanamorais13 <joanamorais13@gmail.com>
2024-01-31 11:49:54 +0000

    added all cities option

--- src/script.js
+++ src/script.js
@@ -32,8 +32,10 @@ function updateCity (event){
     <h2>${cityName}</h2>
     <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div>
-    <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
-    </div>`;
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+    </div>
+    <a href class="all-cities"="/">All cities</a>
+    `;
 }
 
 
